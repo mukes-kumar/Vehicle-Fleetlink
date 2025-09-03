a08235016d3044af0d06e384bf6b888f785e8c18
Mukesh Prajapati <145520335+mukes-kumar@users.noreply.github.com>
2025-09-03 23:09:21 +0530

    api baser url add

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api' // <- adjust if your API base differs
+  // baseURL: 'http://localhost:5000/api' // <- adjust if your API base differs
+  baseURL: 'https://rentmyrider-car-server.vercel.app/api'
 });
 
 // attach token automatically
